d69a5dd671cc56e2912f224ca03f78322dd731af
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-11-14 13:20:25 +0200

    add temporary json file

--- client/src/api/getData.ts
+++ client/src/api/getData.ts
@@ -2,9 +2,13 @@ import axios from "axios";
 import { Job } from "../store/Jobs/types";
 import config from "./token";
 
+// export const getAllJobs = () => {
+//   return axios.get<Job[]>(
+//     "https://api.json-generator.com/templates/ZM1r0eic3XEy/data",
+//     config
+//   );
+// };
+
 export const getAllJobs = () => {
-  return axios.get<Job[]>(
-    "https://api.json-generator.com/templates/ZM1r0eic3XEy/data",
-    config
-  );
+  return axios.get<Job[]>("data.json");
 };
