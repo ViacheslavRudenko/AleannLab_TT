b9e92190f0742cf050ea990ed62471dc1b66fbdd
Viacheslav Rudenko <slavikrudenko@gmail.com>
2022-11-15 20:45:24 +0200

    sm changes

--- client/server.js
+++ client/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.resolve(__dirname, "build")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 app.listen(PORT, (err) => {
